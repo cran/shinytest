fb18c5f7b9db2991e37fc51d72b1f818cba226d8
Winston Chang <winston@rstudio.com>
2023-03-22 21:10:02 +0000

    version 1.5.2

--- R/on-load.R
+++ R/on-load.R
@@ -4,6 +4,21 @@
 .onLoad <- function(libname, pkgname) {
   debugme()
 
-  # This will issue a message if phantomjs isn't found.
-  find_phantom()
+  # This will issue a message if phantomjs isn't found, converting the regular
+  # message to a packageStartupMessage.
+  convert_message_to_package_startup_message({
+    find_phantom()
+  })
+}
+
+# Evaluate an expression, and if it emits any messsages, convert them to
+# packageStartupMessage.
+convert_message_to_package_startup_message <- function(expr) {
+  withCallingHandlers(
+    force(expr),
+    message = function(cnd) {
+      packageStartupMessage(conditionMessage(cnd))
+      tryInvokeRestart("muffleMessage")
+    }
+  )
 }
